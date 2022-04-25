82b4201eeb21d3c0ea7e0eb7e0831a9291f82abf
Justine Kate Albero <91230205+JustineKateAlbero@users.noreply.github.com>
2022-04-25 15:43:36 +0800

    Add files via upload

---
+++ Midterm Exam (Problem 2).py
@@ -0,0 +1,25 @@
+from tkinter import *
+
+window = Tk()
+window.geometry("600x400")
+window.title("Midterm in OOP")
+
+lbl1 = Label(window, text = "Enter your fullname:", fg="red")
+lbl1.place(x= 40, y=100)
+
+v1= StringVar()
+v2= StringVar()
+
+def ButtonCommand():
+    v2.set(v1.get())
+txtfld1 = Entry(window,bd= 4, font = ("Verdana",15), textvariable=v1)
+txtfld1.place(x= 260, y= 100)
+
+btn1 = Button(window, text = "Click to display your fullname:", fg="red", command=lambda:ButtonCommand())
+btn1.place(x= 40, y= 155)
+
+txtfld2 = Entry(window,bd= 4, font = ("Verdana",15), textvariable=v2)
+txtfld2.place(x= 260, y= 150)
+
+
+window.mainloop()
